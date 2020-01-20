02804412fdeeb4d0f82563c641b088dddf985c57
rlawlstjd0077 <rlawlstjd077@gmail.com>
2020-01-20 21:42:53 +0900

    Profile 추가

---
+++ src/main/java/com/jinseong/soft/springboot/web/ProfileController.java
@@ -0,0 +1,27 @@
+package com.jinseong.soft.springboot.web;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class ProfileController {
+    private final Environment env;
+
+    @GetMapping("/profile")
+    public String profile() {
+        //현재 실행중인 ActiveProfile을 모두 가져옵니다.
+        //즉, real, oauth, real-db 등이 활성화 되어 있다면 3개가 모두 담겨 있습니다.
+        //여기서 real, real1, real2는 모두 배포에 사용될 profile이라 이 중 하나라도 있으면 그 값을 반환하도록 합니다.
+        List<String> profiles = Arrays.asList(env.getActiveProfiles());
+        List<String> realProfile = Arrays.asList("real", "real1", "real2");
+        String defaultProfile = profiles.isEmpty()? "default" : profiles.get(0);
+
+        return profiles.stream().filter(realProfile::contains).findAny().orElse(defaultProfile);
+    }
+}
---
+++ src/test/java/com/jinseong/soft/springboot/ProfileControllerUnitTest.java
@@ -0,0 +1,57 @@
+package com.jinseong.soft.springboot;
+
+import com.jinseong.soft.springboot.web.ProfileController;
+import org.junit.Test;
+import org.springframework.mock.env.MockEnvironment;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProfileControllerUnitTest {
+
+    @Test
+    public void real_profile이_조회된다() {
+        //given
+        String expectedProfile = "real";
+        MockEnvironment environment = new MockEnvironment();
+        environment.addActiveProfile(expectedProfile);
+        environment.addActiveProfile("oauth");
+        environment.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(environment);
+        String profile = controller.profile();
+
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void real_profile이_없으면_첫번째가_조회된다() {
+        //given
+        String expectedProfile = "oauth";
+        MockEnvironment env = new MockEnvironment();
+
+        env.addActiveProfile(expectedProfile);
+        env.addActiveProfile("real-db");
+
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+
+    @Test
+    public void active_profile이_없으면_default가_조회된다() {
+        //given
+        String expectedProfile = "default";
+        MockEnvironment env = new MockEnvironment();
+        ProfileController controller = new ProfileController(env);
+
+        //when
+        String profile = controller.profile();
+
+        //then
+        assertThat(profile).isEqualTo(expectedProfile);
+    }
+}
